e3f66e4ab0892c932df1e960c323b6d923ab28c4
Ryan Ware <ryan.ware1987@gmail.com>
2017-11-22 11:46:24 +0000

    Setup for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn we_are_social.wsgi:application
+worker: python worker.py
--- settings/dev.py
+++ settings/dev.py
@@ -4,9 +4,11 @@ DEBUG = True
 
 INSTALLED_APPS.append('debug_toolbar')
 
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backend.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
